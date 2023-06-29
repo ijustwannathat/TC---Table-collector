6b480e804add3294fcc8e5864c0d64079d32b6f7
ijustwannathat <kokfbc@gmail.com>
2023-06-29 19:57:35 +0200

    rest framework created api"

---
+++ store_rest_api/core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import PostSerializer
+from .models import Post
+from rest_framework.response import Response
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+
